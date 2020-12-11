4eabf6c3963a9f6da66a2c3b0054ad1e8fc40179
Diogo <diogo.cortez@autoavaliar.com.br>
2020-12-10 23:56:37 -0300

    First commit

---
+++ src/Controller/ArticlesController.php
@@ -0,0 +1,106 @@
+<?php
+namespace App\Controller;
+
+use App\Controller\AppController;
+
+/**
+ * Articles Controller
+ *
+ * @property \App\Model\Table\ArticlesTable $Articles
+ *
+ * @method \App\Model\Entity\Article[]|\Cake\Datasource\ResultSetInterface paginate($object = null, array $settings = [])
+ */
+class ArticlesController extends AppController
+{
+    /**
+     * Index method
+     *
+     * @return \Cake\Http\Response|null
+     */
+    public function index()
+    {
+        $articles = $this->paginate($this->Articles);
+
+        $this->set(compact('articles'));
+    }
+
+    /**
+     * View method
+     *
+     * @param string|null $id Article id.
+     * @return \Cake\Http\Response|null
+     * @throws \Cake\Datasource\Exception\RecordNotFoundException When record not found.
+     */
+    public function view($id = null)
+    {
+        $article = $this->Articles->get($id, [
+            'contain' => [],
+        ]);
+
+        $this->set('article', $article);
+    }
+
+    /**
+     * Add method
+     *
+     * @return \Cake\Http\Response|null Redirects on successful add, renders view otherwise.
+     */
+    public function add()
+    {
+        $article = $this->Articles->newEntity();
+        if ($this->request->is('post')) {
+            $article = $this->Articles->patchEntity($article, $this->request->getData());
+            if ($this->Articles->save($article)) {
+                $this->Flash->success(__('O artigo foi salvo.'));
+
+                return $this->redirect(['action' => 'index']);
+            }
+            $this->Flash->error(__('Não é possível adicionar o seu artigo.'));
+        }
+        $this->set(compact('article'));
+    }
+
+    /**
+     * Edit method
+     *
+     * @param string|null $id Article id.
+     * @return \Cake\Http\Response|null Redirects on successful edit, renders view otherwise.
+     * @throws \Cake\Datasource\Exception\RecordNotFoundException When record not found.
+     */
+    public function edit($id = null)
+    {
+        $article = $this->Articles->get($id, [
+            'contain' => [],
+        ]);
+        if ($this->request->is(['patch', 'post', 'put'])) {
+            $article = $this->Articles->patchEntity($article, $this->request->getData());
+            if ($this->Articles->save($article)) {
+                $this->Flash->success(__('O Artigo foi atualizado.'));
+
+                return $this->redirect(['action' => 'index']);
+            }
+            $this->Flash->error(__('O artigo não pôde ser atualizado.'));
+        }
+        $this->set(compact('article'));
+    }
+
+    /**
+     * Delete method
+     *
+     * @param string|null $id Article id.
+     * @return \Cake\Http\Response|null Redirects to index.
+     * @throws \Cake\Datasource\Exception\RecordNotFoundException When record not found.
+     */
+    public function delete($id = null)
+    {
+        $this->request->allowMethod(['post', 'delete']);
+        $article = $this->Articles->get($id);
+        if ($this->Articles->delete($article)) {
+            $this->Flash->success(__('O artigo foi deletado.'));
+        } else {
+            $this->Flash->error(__('O artigo não pode ser deletado.'));
+        }
+
+        return $this->redirect(['action' => 'index']);
+    }
+}
